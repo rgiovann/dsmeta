85ebb84a55af8b940e471c23f26b3ee43a1c53c3
Giovani L. Rozza <giovanni.rozza@gmail.com>
2022-11-14 23:21:22 -0300

    React Hook useState to persist data input changes.

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -3,24 +3,32 @@ import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 
 import './styles.css';
+import { useState } from 'react';
 
 function SalesCard() {
+    const [minDate, setMinDate] = useState(new Date());
+    const [maxDate, setMaxDate] = useState(new Date());
+
     return (
         <div className="dsmeta-card">
             <h2 className="dsmeta-sales-title">Vendas</h2>
             <div>
                 <div className="dsmeta-form-control-container">
                     <DatePicker
-                        selected={new Date()}
-                        onChange={(date: Date) => {}}
+                        selected={minDate}
+                        onChange={(date: Date) => {
+                            setMinDate(date);
+                        }}
                         className="dsmeta-form-control"
                         dateFormat="dd/MM/yyyy"
                     />
                 </div>
                 <div className="dsmeta-form-control-container">
                     <DatePicker
-                        selected={new Date()}
-                        onChange={(date: Date) => {}}
+                        selected={maxDate}
+                        onChange={(date: Date) => {
+                            setMaxDate(date);
+                        }}
                         className="dsmeta-form-control"
                         dateFormat="dd/MM/yyyy"
                     />
