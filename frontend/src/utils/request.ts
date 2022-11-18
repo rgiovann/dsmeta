9cf4bc4a1491460bcef7d5efaa0d521472b7cef1
Giovani L. Rozza <giovanni.rozza@gmail.com>
2022-11-18 12:58:56 -0300

    Sales list.

---
+++ frontend/src/utils/request.ts
@@ -0,0 +1,5 @@
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL ?? "http://localhost:8080";
+/* 
+enviromnent var to request data from backend, if not in production default value is
+http://localhost:8080
+*/
